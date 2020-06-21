a35a377a0c2724229266aa433ff341ce98e4fcbe
airtonferreira <airton.felix@dcx.ufpb.br>
2020-06-21 19:20:39 -0300

    Quiz Service

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,12 @@ import './quiz.dart';
 main() => runApp(QuizApp());
 
 class _QuizAppState extends State<QuizApp> {
-  var _selectedQuestion = 0;
-
-  bool get haveSelectedQuestion {
-    return _selectedQuestion < _questions.length;
-  }
-
-  void _response() {
-    setState(() {
-      _selectedQuestion++;
-    });
-  }
-
-  final _questions = const [
-    {
-      'question': 'Qual propriedade CSS centraliza um elemento?',
-      'responses': ['Align Items', 'Text Align', 'Justify Content'],
-    },
-    {
-      'question': 'Qual tag HTML tem propriedades de mídia?',
-      'responses': ['div', 'video', 'iframe'],
-    }
-  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: Text('Quiz DEV')),
-          body: haveSelectedQuestion
-              ? Quiz(
-                  questions: _questions,
-                  selectedQuestion: _selectedQuestion,
-                  response: _response,
-                )
-              : Result()),
+      title: 'quizDEV',
+      home: QuizView(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/services/quiz_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:projeto_quiz/models/question.dart';
+
+class QuizService {
+  static Future<List<Question>> fetch() async {
+    try {
+      var url =
+          'https://script.google.com/macros/s/AKfycbwt2bnmO7Z1LrpPFmQI5w8YHQinfZekVbrpVXMUb-bdx4CHT6A/exec';
+      var res = await http.get(url);
+
+      if (res.statusCode == 200) {
+        var data = json.decode(res.body);
+        return List<Question>.from(
+            data['questions'].map((x) => Question.fromMap(x)));
+      } else {
+        return List<Question>();
+      }
+    } catch (error) {
+      print(error);
+      return List<Question>();
+    }
+  }
+}
